32596b9fa2d0effc4ecec7985a268838322b90b1
aplikasi365 <aplikasi365@gmail.com>
2018-03-27 04:59:25 +0700

    First Initial

---
+++ app/src/main/java/com/smartcrm/krangganmarket/smartcrm/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.smartcrm.krangganmarket.smartcrm.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.smartcrm.krangganmarket.smartcrm.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
